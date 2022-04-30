f046350af4f03563ba70b4aac3c501a462970c06
guillaume <guillaume.ramelet@gmail.com>
2022-04-30 17:47:03 +0200

    2022-04-30_17:46

--- files/setup_wsl_02_install_python_conda_part2.sh
+++ files/setup_wsl_02_install_python_conda_part2.sh
@@ -1,4 +1,4 @@
-echo "configure base v3"
+echo "configure base v4"
 conda install -y mamba -n base -c conda-forge
 mamba init
 mamba install -y nb_conda_kernels
@@ -7,14 +7,17 @@ tee -a ~/.bashrc << EOF
 export REQUESTS_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt
 EOF
 
-echo "conda=mamba" >> $HOME/.bash_aliases
+tee -a ~/.bashrc << EOF
+function conda() {
+mamba "$@";
+}
+export -f conda
+EOF
 
 if [ -e "/.cfg" ]; then
 		config='/usr/bin/git --git-dir=/.cfg/ --work-tree=/'
 		$config add ~/.bashrc
-		$config commit -m'export certificates for conda'
-		$config add ~/.bash_aliases
-		$config commit -m'alias mamba'	
+		$config commit -m'export certificates for conda, and conda as mamba'
 		$config push		
 fi
 
