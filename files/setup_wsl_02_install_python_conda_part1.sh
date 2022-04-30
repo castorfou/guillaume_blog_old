16eec6d1ef53920b8298d5fc66fb1a462787afca
guillaume <guillaume.ramelet@gmail.com>
2022-04-30 11:13:56 +0200

    2022-04-30_11:13

--- files/setup_wsl_02_install_python_conda_part1.sh
+++ files/setup_wsl_02_install_python_conda_part1.sh
@@ -1,6 +1,7 @@
-echo "install conda"
+echo "install conda v1"
 tmp_dir=$(mktemp -d )
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -P $tmp_dir
+chmod +x $tmp_dir/Miniconda3-latest-Linux-x86_64.sh
 $tmp_dir/Miniconda3-latest-Linux-x86_64.sh -f -b -p ~/miniconda
 ~/miniconda/bin/conda init
 # create a config file
