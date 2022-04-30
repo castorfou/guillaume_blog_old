5fbf794a79005528a6114527381090fa170dc893
guillaume <guillaume.ramelet@gmail.com>
2022-04-30 11:08:22 +0200

    2022-04-30_11:08

--- files/setup_wsl_02_install_python_conda_part1.sh
+++ files/setup_wsl_02_install_python_conda_part1.sh
@@ -1,8 +1,7 @@
 echo "install conda"
-if [ ! -e "~/.condarc" ]; then
 tmp_dir=$(mktemp -d )
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -P $tmp_dir
-sudo bash $tmp_dir/Miniconda3-latest-Linux-x86_64.sh -f -b -p ~/miniconda
+$tmp_dir/Miniconda3-latest-Linux-x86_64.sh -f -b -p ~/miniconda
 ~/miniconda/bin/conda init
 # create a config file
 tee ~/.condarc << EOF
@@ -10,4 +9,3 @@ ssl_verify: false
 shortcuts: false
 report_errors: false
 EOF
-fi
