183a6dde455ea6e51be2984c6c6b22b96a11092d
castorfou <castorfou@users.noreply.github.com>
2022-04-30 09:11:28 +0000

    deploy: 00ce09fe4f6ae48b067a2f157d8d64874a836f44

--- files/setup_wsl_02_install_python_conda_part2.sh
+++ files/setup_wsl_02_install_python_conda_part2.sh
@@ -1,8 +1,8 @@
 echo "configure base"
-conda install mamba -n base -c conda-forge
+conda install -y mamba -n base -c conda-forge
 mamba init
-mamba install nb_conda_kernels
-mamba install -c conda-forge jupyterlab jupyterlab-git
+mamba install -y nb_conda_kernels
+mamba install -y -c conda-forge jupyterlab jupyterlab-git
 tee -a ~/.bashrc << EOF
 export REQUESTS_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt
 EOF
