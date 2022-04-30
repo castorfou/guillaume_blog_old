5a503efd634e37894ae931be90684749250e9868
castorfou <castorfou@users.noreply.github.com>
2022-04-30 07:49:50 +0000

    deploy: 866a5d402450dbab9c752787b54dbd238f78955f

--- files/setup_wsl_user.sh
+++ files/setup_wsl_user.sh
@@ -28,40 +28,6 @@ git clone git@gitlab.michelin.com:devops-foundation/devops_environment.git /tmp/
 sudo cp /tmp/devops_environment/certs/* /usr/local/share/ca-certificates/
 sudo update-ca-certificates
 rm -rf /tmp/devops_environment
-echo
-
-echo "4. update apt sources with artifactory"
-echo 'Acquire { http::User-Agent "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:13.37) Gecko/20100101 Firefox/31.33.7"; };' | sudo tee /etc/apt/apt.conf.d/90globalprotectconf
-sudo sed -i 's,http://archive.ubuntu.com/ubuntu,https://artifactory.michelin.com/artifactory/ubuntu-archive-remote,g' /etc/apt/sources.list
-sudo sed -i 's,http://security.ubuntu.com/ubuntu,https://artifactory.michelin.com/artifactory/ubuntu-archive-remote,g' /etc/apt/sources.list
-sudo apt update
-sudo apt upgrade -y
-echo
-
-echo "5. automount secured vbox"
-sudo apt install -y autofs
-mkdir -p ~/vbox
-if ! grep -Fxq "vbox" /etc/auto.master
-then
-sudo tee -a /etc/auto.master << EOF
-/home/$USER/vbox            /etc/vbox.autofs        --timeout=30 browse
-EOF
-fi
-if [ ! -e "/etc/vbox.autofs" ]; then
-sudo tee /etc/vbox.autofs << EOF
-janus   -fstype=drvfs,uid=1000,gid=1000 :Z:
-cho     -fstype=drvfs,uid=1000,gid=1000 :Y:
-EOF
-echo "`whoami` ALL=(ALL) NOPASSWD: /usr/sbin/service" | sudo tee /etc/sudoers.d/`whoami` && sudo chmod 0440 /etc/sudoers.d/`whoami`
-fi
-sudo service autofs start
-if ! grep -Fxq "autofs" ~/.bashrc
-then
-echo 'sudo /usr/sbin/service autofs start' >> ~/.bashrc
-fi
-echo
-
-echo "6. install conda and configure base"
 if [ $DISTRIB_RELEASE == "22.04" ]
 then
 echo 'bug SSL with ubuntu 22.04 - https://bugs.launchpad.net/ubuntu/+source/openssl/+bug/1963834/comments/7'
@@ -76,23 +42,13 @@ system_default = system_default_sect
 Options = UnsafeLegacyRenegotiation
 EOF
 fi
-if [ ! -e "~/.condarc" ]; then
-tmp_dir=$(mktemp -d )
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -P $tmp_dir
-sudo bash $tmp_dir/Miniconda3-latest-Linux-x86_64.sh -f -b -p ~/miniconda
-~/miniconda/bin/conda init
-# create a config file
-tee ~/.condarc << EOF
-ssl_verify: false
-shortcuts: false
-report_errors: false
-EOF
-source ./.bashrc
-conda install mamba -n base -c conda-forge
-mamba init
-mamba install nb_conda_kernels
-mamba install -c conda-forge jupyterlab jupyterlab-git
-tee -a ~/.bashrc << EOF
-export REQUESTS_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt
-EOF
-fi
+echo
+
+echo "4. update apt sources with artifactory"
+echo 'Acquire { http::User-Agent "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:13.37) Gecko/20100101 Firefox/31.33.7"; };' | sudo tee /etc/apt/apt.conf.d/90globalprotectconf
+sudo sed -i 's,http://archive.ubuntu.com/ubuntu,https://artifactory.michelin.com/artifactory/ubuntu-archive-remote,g' /etc/apt/sources.list
+sudo sed -i 's,http://security.ubuntu.com/ubuntu,https://artifactory.michelin.com/artifactory/ubuntu-archive-remote,g' /etc/apt/sources.list
+sudo apt update
+sudo apt upgrade -y
+echo
+
